3c5c7418206ad4900b38a39dd2561ab098285b18
羊羊 <1690374201@qq.com>
2019-01-30 23:45:48 +0800

    创建、查找、删除评论

---
+++ models/comment.js
@@ -0,0 +1,31 @@
+let Base = require('./base')
+
+class Comment extends Base{
+
+  findById(params, cb) {
+    let sqlStr = 'SELECT user_id,is_delete FROM ety_comment WHERE id = ?'
+    this.query(sqlStr, params, cb)
+  }
+
+  findAllByArticleId(params,cb){
+    // 删了的不拿
+    let sqlStr = 'SELECT user_id,is_delete,content FROM ety_comment WHERE article_id = ? AND is_delete=0'
+    this.query(sqlStr, params, cb)
+  }
+
+  create(params, cb) {
+    let sqlStr = 'INSERT INTO ety_comment (user_id,content,article_id,create_time) VALUES (?,?,?,now())'
+    let {user_id,content,article_id,create_time} = params
+    let sqlParams = [user_id,content,article_id,create_time]
+    this.query(sqlStr, sqlParams, cb)
+  }
+
+  deleteById(sqlParams, cb) {
+    let sqlStr = 'UPDATE ety_comment SET is_delete = 1,delete_time = now() WHERE id = ?'
+    this.query(sqlStr, sqlParams, cb)
+  }
+
+}
+
+
+module.exports = Comment
--- routes/route.js
+++ routes/route.js
@@ -2,9 +2,12 @@ let express = require('express')
 let router = express.Router()
 let User = require('./../models/user')
 let Article = require('./../models/article')
+let Comment = require('./../models/comment')
+
 
 let user = new User()
 let article = new Article()
+let comment = new Comment()
 
 
 /* GET user  */
@@ -125,8 +128,8 @@ router.get('/api/article/create', function (req, res, next) {
   }
 
   let userId = req.session.userId  // 如果有cookie,就不需要userId
-  let title = req.param('title')
-  let content = req.param('content')
+  let title = (req.param('title') || '').trim()
+  let content = (req.param('content') || '').trim()
   let tagList = (req.param('tagList') || []).join(',')
   let categoryId = req.param('categoryId')
 
@@ -149,8 +152,8 @@ router.get('/api/article/create', function (req, res, next) {
 
 router.get('/api/article/update', function (req, res, next) {
   let articleId = req.param('articleId')
-  let title = req.param('title')
-  let content = req.param('content')
+  let title = (req.param('title') || '').trim()
+  let content = (req.param('content') || '').trim()
   let tagList = (req.param('tagList') || []).join(',')
   let categoryId = req.param('categoryId')
 
@@ -196,6 +199,65 @@ router.get('/api/article/detail', function (req, res, next) {
       })
       res.send(result[0])
     }
+  })
+})
+
+
+// TODO 这个接口是不是叫 /api/article/comment 更好？？
+router.get('/api/comment/create', function (req, res, next) {
+  // TODO 这种需要登录的 统一弄个地方拦截吧  单个的路由里先不处理
+  let articleId = req.param('articleId')
+  let content = (req.param('content') || '').trim()
+  let userId = req.session.userId
+
+  if (!articleId) {
+    return res.send('params error,need articleId')
+  }
+
+  if (!content) {
+    return res.send('content cant be empty')
+  }
+
+  //  TODO 数据库的下划线  混在一起好丑怎么办   要写个函数统一处理吗？   但是如果万一有例外怎么办
+  let params = {
+    article_id: articleId,
+    content,
+    user_id: userId,
+  }
+  comment.create(params, function () {
+    res.send('create comment success')
+  })
+})
+
+
+// TODO 应该叫 /api/article/comments ???
+router.get('/api/comment/getList', function (req, res, next) {
+  //  有两个参数  一个是articleId  另个是 userId 可以获取一个人发出的所有评论  以后做
+  //  要不要分成两个接口
+
+  let articleId = req.param('articleId')
+  comment.findAllByArticleId(articleId, function (result) {
+    res.send(result)
+  })
+  // TODO 这里应该要分页  size page   先凑合着，以后加
+})
+
+
+router.get('/api/comment/delete', function (req, res, next) {
+  let id = req.param('id')
+  let userId = req.session.userId
+  comment.findById(id,function (result) {
+    if(!result.length){
+      return res.send('cant find such a comment')
+    }
+    if(result[0].user_id !== userId){
+      return res.send('Permission denied,its not your comment')
+    }else if(result[0].is_delete === 1){
+      return res.send('this comment has been deleted already')
+    }
+    comment.deleteById(id,function () {
+      return res.send('delete success')
+    })
   })
 })
 
